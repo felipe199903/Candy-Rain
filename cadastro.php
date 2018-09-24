189a59c0cf69088cc125a9ce04ff74ab68477650
felipe199903 <schorles21@gmail.com>
2018-09-24 20:32:46 -0300

    php submit

--- cadastro.php
+++ cadastro.php
@@ -1,8 +1,32 @@
-<html>
-<body>
-
-Cadastro <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
-</html>
+<?php
+ 
+ 
+$nome = $_POST['txtName'];
+$email = $_POST['txtEmail'];
+$phone = $_POST['txtPhone'];
+$msg = $_POST['txtMsg'];
+ 
+ 
+$headers = "From: ". $nome;
+ 
+$corpoemail = '<b>Fale Conosco</b>
+             
+               Nome: '   .$nome.' /n
+               Email:'   .$email.'/n
+               Telefone:' .$phone.' /n
+               Mensagem:'.$msg.' /n';
+ 
+ 
+ 
+ 
+if(mail("schorles21@gmail.com", "Fale Conosco",$corpoemail,$headers)){
+ 
+ 
+       echo "<script>alert('Mensagem enviada com sucesso!');</script>"; 
+       header("Location: index.php");
+ 
+} else{
+ 
+      echo "<script>alert('Erro ao enviar, tente diretamente pelo email schorles21@gmail.com');</script>";  
+ 
+}
